6d8fdbf545811d58816a8786248aa6e634fe6751
Luigi Iossa <luigi_iossa@hotmail.com>
2024-01-19 16:22:05 +0000

    navbar

--- src/Components/NavBarTop.jsx
+++ src/Components/NavBarTop.jsx
@@ -1,36 +1,26 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
-function NavBarTop () {
-    return (
-        <>
-        <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-        </>
-    )
+
+function NavBarTop() {
+  return (
+    <>
+      <Navbar expand="lg" className="bg-body-tertiary mt-3 mx-5 border border-dark">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" className='border border-0 rounded-0' />
+          <Navbar.Collapse id="basic-navbar-nav" >
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Menus</Nav.Link>
+              <Nav.Link href="#link">Hours & Location</Nav.Link>
+              <Nav.Link href="#link">Reservation</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  )
 }
 
 export default NavBarTop;
